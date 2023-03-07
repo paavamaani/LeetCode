13e08446d12250048274fcb01e38bc56a55ceae5
Paavamaani Manchala <paavamaanimanchala@gmail.com>
2023-03-07 12:34:01 -0800

    Designed HashSet

---
+++ DesignHashSet.java
@@ -0,0 +1,55 @@
+#https://leetcode.com/problems/design-hashset/description/
+
+class MyHashSet {
+    private int primaryBucketSize;
+    private int secondaryBucketSize;
+    private boolean[][] bucket;
+
+    public MyHashSet() {
+        this.primaryBucketSize = 1000;
+        this.secondaryBucketSize = 1000;
+        this.bucket = new boolean[primaryBucketSize][];
+    }
+    
+    public int primaryBucketHashKey(int key) {
+        return key % primaryBucketSize;
+    }
+
+    public int secondaryBucketHashKey(int key) {
+        return key / secondaryBucketSize;
+    }
+
+    public void add(int key) {
+        int primaryHashKey = primaryBucketHashKey(key);
+        int secondaryHashKey = secondaryBucketHashKey(key);
+
+        if(bucket[primaryHashKey] == null) {
+            if(primaryHashKey == 0) {
+                bucket[primaryHashKey] = new boolean[secondaryBucketSize + 1];
+            }
+            else {
+                bucket[primaryHashKey] = new boolean[secondaryBucketSize];
+            }
+        }
+
+        bucket[primaryHashKey][secondaryHashKey] = true;    
+    }
+    
+    public void remove(int key) {
+        int primaryHashKey = primaryBucketHashKey(key);
+        int secondaryHashKey = secondaryBucketHashKey(key);
+
+        if(bucket[primaryHashKey] == null) return;
+        
+        bucket[primaryHashKey][secondaryHashKey] = false;
+    }
+    
+    public boolean contains(int key) {
+        int primaryHashKey = primaryBucketHashKey(key);
+        int secondaryHashKey = secondaryBucketHashKey(key);
+
+        if(bucket[primaryHashKey] == null) return false;
+
+        return bucket[primaryHashKey][secondaryHashKey];
+    }
+}

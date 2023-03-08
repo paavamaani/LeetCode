b107c476683b2ab205bad5d995df91cd1e738182
Paavamaani Manchala <paavamaanimanchala@gmail.com>
2023-03-07 16:45:28 -0800

    Implemented Queue using stack

---
+++ QueueUsingStack.java
@@ -0,0 +1,39 @@
+#https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    public void isStackEmpty() {
+        if(stack2.isEmpty()) {
+           Iterator value = stack1.iterator();
+
+           while(value.hasNext()) {
+               stack2.push(stack1.pop());
+           }
+        }
+    }
+    
+    public int pop() {
+        isStackEmpty();
+        return stack2.pop();
+    }
+    
+    public int peek() {
+        isStackEmpty();
+        return stack2.peek();
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}

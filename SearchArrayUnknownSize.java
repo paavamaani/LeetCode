c7955b2e5e4fe659b0896cf050bc7a53b9efeb4a
Paavamaani Manchala <paavamaanimanchala@gmail.com>
2023-03-09 22:19:47 -0800

    Implemented search in  array with unknown size

---
+++ SearchArrayUnknownSize.java
@@ -0,0 +1,31 @@
+#https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        if(reader == null) return -1;
+
+        int low = 0;
+        int high = 1;
+
+        while(reader.get(high) < target) {
+            low = high;
+            high = high * 2;
+        }
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(reader.get(mid) == target) {
+                return mid;
+            }
+            else if(reader.get(mid) < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
